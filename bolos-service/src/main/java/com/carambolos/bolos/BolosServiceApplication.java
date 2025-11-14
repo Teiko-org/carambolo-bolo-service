16bd66440eb1e424a997786bb2a10105e5a4bc22
cantalejomatheus <matheus.braga@sptech.school>
2025-11-14 19:00:18 -0300

    commit inicial do projeto

---
+++ bolos-service/src/main/java/com/carambolos/bolos/BolosServiceApplication.java
@@ -0,0 +1,13 @@
+package com.carambolos.bolos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolosServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolosServiceApplication.class, args);
+	}
+
+}
